a25b72b8faa650086b197a7f47f542df8987c2cd
Tony Tam <fehguy@gmail.com>
2012-08-20 15:30:49 -0700

    fixed bad arg

--- bin/runscala.sh
+++ bin/runscala.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORDNIK_OPTS=${WORDNIK_OPTS:=NOPE}
-
-if [ $WORDNIK_OPTS = NOPE ];
-  then
-    export WORDNIK_OPTS="-DconfigFile=conf/config.xml -DdevMode=true"
-  
-fi
-
 export CLASSPATH="$DIR/../target/lib/*:$DIR/../target/*"
-export JAVA_OPTS="${JAVA_OPTS} -DrulePath=data -Xmx4096M"
-scala $WORDNIK_OPTS $JAVA_CONFIG_OPTIONS -cp $CLASSPATH "$@"
+export JAVA_OPTS="${JAVA_OPTS} -DXmx4096M"
+scala $WORDNIK_OPTS $JAVA_OPTS -cp $CLASSPATH "$@"
