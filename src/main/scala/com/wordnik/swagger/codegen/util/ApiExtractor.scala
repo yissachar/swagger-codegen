d01233bf5eeaa32e9a860606a7ba229b0ac99c3a
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-03-10 14:59:38 +0100

    Refactor api extractor, removes redundant pattern matches on booleans

--- src/main/scala/com/wordnik/swagger/codegen/util/ApiExtractor.scala
+++ src/main/scala/com/wordnik/swagger/codegen/util/ApiExtractor.scala
@@ -26,46 +26,40 @@ import java.io.InputStream
 
 import scala.io._
 import scala.collection.mutable.{ ListBuffer, HashMap, HashSet }
+import util.control.Exception.allCatch
 
 object ApiExtractor extends RemoteUrl {
   implicit val formats = SwaggerSerializers.formats
 
+  private[this] def safeNotFound(basePath: String, api: ApiListingReference)(t: Throwable): Option[ApiListing] = t match {
+    case e:java.io.FileNotFoundException =>
+      println("WARNING!  Unable to read API " + basePath + api.path)
+      None
+    case _ => None
+  }
+
+  private[this] def readJson(basePath: String, api: ApiListingReference, apiKey: Option[String] = None): String = {
+    if (basePath.startsWith("http")){
+      println("calling: " + ((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json")))
+      urlToString((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json"))
+    } else Source.fromFile((basePath + api.path).replaceAll(".\\{format\\}", ".json")).mkString
+  }
+
   def fetchApiListings(basePath: String, apis: List[ApiListingReference], apiKey: Option[String] = None): List[ApiListing] = {
-    (for (api <- apis) yield {
-          try{
-            val json = basePath.startsWith("http") match {
-              case true => {
-                println("calling: " + ((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json")))
-                urlToString((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json"))
-              }
-              case false => Source.fromFile((basePath + api.path).replaceAll(".\\{format\\}", ".json")).mkString
-            }
-            Some(parse(json).extract[ApiListing])
-          }
-          catch {
-            case e:java.io.FileNotFoundException => {
-              println("WARNING!  Unable to read API " + basePath + api.path)
-              None
-            }
-            case _ : Throwable=> None
-          }
-        }).flatten.toList
+    (for {
+      api <- apis
+      err = safeNotFound(basePath, api)(_)
+      json = readJson(basePath, api, apiKey)
+    } yield allCatch.withApply(err){ parseOpt(json) flatMap (_.extractOpt[ApiListing]) }).flatten
   }
 
   def extractApiOperations(basePath: String, references: List[ApiListingReference], apiKey: Option[String] = None) = {
-    for (api <- references) yield {
-      val json = basePath.startsWith("http") match {
-        case true => {
-          println("calling: " + ((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json")))
-          urlToString((basePath + api.path + apiKey.getOrElse("")).replaceAll(".\\{format\\}", ".json"))
-        }
-        case false => Source.fromFile((basePath + api.path).replaceAll(".\\{format\\}", ".json")).mkString
-      }
-      parse(json).extract[ApiListing]
-    }
+    for {
+      api <- references
+      json = readJson(basePath, api, apiKey)
+    } yield read[ApiListing](json)
   }
 
-  def extractApiOperations(basePath: String, apiDescription: ApiDescription): List[(String, Operation)] = {
-    (for(op <- apiDescription.operations) yield (apiDescription.path, op)).toList
-  }
+  def extractApiOperations(basePath: String, apiDescription: ApiDescription): List[(String, Operation)] =
+    for(op <- apiDescription.operations) yield apiDescription.path -> op
 }
