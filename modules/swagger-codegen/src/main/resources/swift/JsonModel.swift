063e9d73b16ff830343f95c80488692729c65b9f
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2015-03-31 14:50:50 -0400

    Fix ser/deser bugs

--- modules/swagger-codegen/src/main/resources/swift/JsonModel.swift
+++ modules/swagger-codegen/src/main/resources/swift/JsonModel.swift
@@ -1,27 +1,7 @@
-class JsonModel: NSObject {
-
-  init(json: NSDictionary = NSDictionary()) {
-    super.init()
-
-    let properties: [String: MirrorType] = getProperties()
-    for (jsonKey, jsonValue) in json {
-      let key: String = jsonKey as String
-      
-      if (respondsToSelector(NSSelectorFromString(key))) {
-        var value: AnyObject? = jsonValue
-        let mirrorType = properties[key]
-
-        if value is NSDictionary || value is NSArray {
-          value = ApiInvoker.deserialize(jsonValue, type: mirrorType!.valueType)
-        }
-
-        setValue(value, forKey: key)
-      }
-    }
-  }
+class JsonModel {  
   
   func toJson() -> String {        
-    return jsonify(self)
+    fatalError("This method must be overridden")
   }
 
   func getProperties() -> [String: MirrorType] {
@@ -31,7 +11,7 @@ class JsonModel: NSObject {
     var numProperties = reflected.count
     for index in 0..<numProperties {
       let (property, mirror) = reflected[index]
-      if property != "super" && property != "id" {
+      if property != "super" {
         properties[property] = mirror
       }
     }
@@ -39,38 +19,26 @@ class JsonModel: NSObject {
     return properties
   }
 
-  func jsonify(object: AnyObject) -> String {
+  func jsonify(object: AnyObject?) -> String {
     var json: String = ""
 
-    if object is String {
-      json += "\"\(object)\""
-    } else if object is JsonModel {
-      json += "{"
-        
-      var prefix: String = ""
-      for property in (object as JsonModel).getProperties().keys {
-        if let value: AnyObject = (object as JsonModel).valueForKey(property) {
-          json += prefix
-          json += "\"\(property)\": "
-          json += jsonify(value)
-          prefix = ","
-        }
-      }
-      
-      json += "}"
-    } else if object is [AnyObject] {
+    if let str = object as? String {
+      json += "\"\(str)\""
+    } else if let model = object as? JsonModel {
+      json += model.toJson()
+    } else if let array = object as? [AnyObject] {
       json += "["
         
       var prefix: String = ""
-      for item in (object as [AnyObject]) {
+      for item in array {
         json += prefix
         json += jsonify(item)
         prefix = ","
       }
       
       json += "]"
-    } else {
-      json += "\(object)"
+    } else if object != nil {
+      json += "\(object!)"
     }
 
     return json
